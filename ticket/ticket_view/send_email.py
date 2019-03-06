e409f79d0e00fbdc5a2be0cc64009615264d09ea
CoderDqZhang <769839948@qq.com>
2019-03-06 17:39:34 +0800

    sender email

--- ticket/ticket_view/send_email.py
+++ ticket/ticket_view/send_email.py
@@ -15,8 +15,7 @@ try:
     scheduler.add_jobstore(DjangoJobStore(), "default")
     # 设置定时任务，选择方式为interval，时间间隔为10s
     # 另一种方式为每天固定时间执行任务，对应代码为：
-    # @register_job(scheduler, 'cron', day_of_week='mon-fri', hour='9', minute='30', second='10',id='task_time')
-    @register_job(scheduler,"interval", seconds=10)
+    @register_job(scheduler, 'cron', hour='17', minute='30', second='0',id='task_time')
     def my_job():
         sender_email()
         # 这里写你要执行的任务
