d4358a14ee69738b98896ea013b1be57bb526158
CoderDqZhang <769839948@qq.com>
2019-03-22 11:39:42 +0800

    change choince

--- ticket/ticke_model/account.py
+++ ticket/ticke_model/account.py
@@ -13,15 +13,21 @@ class AccountGroup(models.Model):
     def __str__(self):
         return self.group_name + '-' + self.group_menu
 
+USER_STATUS = (
+    ('0','甲方'),
+    ('1','乙方'),
+)
+
 class Account(models.Model):
     nickname = models.CharField('用户名', max_length=255, default='', null=True) #用户名
+    gender = models.CharField('性别', max_length=7, choices=(('male', u'男'), ('female', '女')), default='female')
     user_id = models.CharField('用户id', primary_key=True, max_length=10,
                                default='', null=False, blank=False) #用户ID
     email = models.EmailField('邮件地址',max_length=254, null=True,blank=True)
     password = models.CharField('密码', max_length=16, default='123456', null=False) #密码
     department = models.ForeignKey(Department,verbose_name='部门',on_delete=models.CASCADE)#多对一（博客--类别）
     group = models.ForeignKey(AccountGroup, verbose_name='群组', on_delete=models.CASCADE,null=True)  # 多对一（博客--类别）
-    status = models.IntegerField('甲乙区分', default = 0, null=True) #甲乙区分 0代表甲方，1代表乙方
+    status = models.CharField('甲乙区分', default = '0', max_length=7, choices=USER_STATUS, null=True) #甲乙区分 0代表甲方，1代表乙方
     desc = models.CharField('负责项目', max_length=255, default='', null=True) #人员介绍
     create_time = models.DateTimeField('创建时间',auto_created=True,auto_now=True) #创建时间
     last_login = models.DateTimeField('最后登录时间',auto_created=False,auto_now=False) #最后登录时间
