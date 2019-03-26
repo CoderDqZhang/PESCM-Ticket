5e1185913adac6444a918b62eba9b42bcdf579a7
CoderDqZhang <769839948@qq.com>
2019-03-26 09:22:27 +0800

    sender admin

--- ticket/ticket_view/send_email.py
+++ ticket/ticket_view/send_email.py
@@ -31,7 +31,7 @@ else:
         # 另一种方式为每天固定时间执行任务，对应代码为：
         # scheduler.add_job(my_job, 'interval', seconds=5)
 
-        scheduler.add_job(my_job, 'cron', day_of_week='mon-fri', hour=8, minute=30, end_date='2099-05-30')
+        scheduler.add_job(my_job, 'cron', day_of_week='mon-fri', hour=17, minute=30, end_date='2099-05-30')
         # @register_job(scheduler, 'cron',day_of_week='mon,tue,wed,thu,fri,', hour='09', minute='55', second='00',id='task_time')
         scheduler.start()
     except Exception as e:
@@ -105,7 +105,7 @@ def sender_email():
                     strs = strs + '工单标题' + ticket.ticket_title + '   ' + '工单编号' + str(ticket.ticket_id) + '\n'
 
                 try:
-                    send_mail('未完成工单提醒', strs,
+                    send_mail('未完成工单提醒(我创建的)', strs,
                       'redbullticket@163.com',
                       senderuser, fail_silently=False)
                 except:
@@ -116,7 +116,7 @@ def sender_email():
                     strs_confirm = strs_confirm + '工单标题' + ticket.ticket_title + '   ' + '工单编号' + str(ticket.ticket_id) + '\n'
 
                 try:
-                    send_mail('未完成工单提醒', strs_confirm,
+                    send_mail('未完成工单提醒(待我执行的)', strs_confirm,
                       'redbullticket@163.com',
                       senderuser, fail_silently=False)
                 except:
@@ -146,7 +146,7 @@ def sender_admin_email():
         for ticket in tickets:
             strs = strs + '工单标题' + ticket.ticket_title + '   ' + '工单编号' + str(ticket.ticket_id) + '\n'
         try:
-            send_mail('未完成工单提醒', strs,
+            send_mail('未完成工单提醒(管理员邮件)', strs,
                       'redbullticket@163.com',
                       senderuser, fail_silently=False)
         except:
