4c92d79f74893ab8290d8bfef98e726c08874c3b
CoderDqZhang <769839948@qq.com>
2019-04-08 11:03:38 +0800

    server change

--- ticket/until/config.py
+++ ticket/until/config.py
@@ -1,6 +1,6 @@
 from ticket.until import define
 
 if (define.get_mac_address() == '00:50:56:82:19:40') :
-    rootUrl = 'http://10.8.8.192:8090'
+    rootUrl = 'http://122.200.85.10:8090'
 else:
     rootUrl = 'http://127.0.0.1:8000'
--- ticket/until/define.py
+++ ticket/until/define.py
@@ -13,7 +13,7 @@ def get_mac_address():
     return ":".join([mac[e:e+2] for e in range(0,11,2)])
 
 if (get_mac_address() == '00:50:56:82:19:40') :
-    MEDIAURL = 'http://10.8.8.192:8090/media/'
+    MEDIAURL = 'http://122.200.85.10:8090/media/'
 else:
     print(get_mac_address())
     MEDIAURL = 'http://127.0.0.1:8000/media/'
